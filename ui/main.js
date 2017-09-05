9bb29fd5d92f4d2d4e316fb69129db2eacc392bd
sairam30011997 <31387961+sairam30011997@users.noreply.github.com>
2017-09-05 17:19:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 
 var element=document.getElementById('main-text');
 element.innerHTML='new value';
+
+var img=document.getElementById('madi');
+img.onclick = function (){
+  img.style.marginLeft = '100px';  
+};
