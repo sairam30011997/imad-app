a84ce773fbecf94ac92c2a20491b62db2ee9e594
sairam30011997 <31387961+sairam30011997@users.noreply.github.com>
2017-09-05 20:59:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,8 +108,8 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/sai ram prakash.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'sai ram prakash.jpg'));
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
