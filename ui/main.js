2508bf6270706c1ce39e2b323b5c44acae7bfaa6
sairam30011997 <31387961+sairam30011997@users.noreply.github.com>
2017-09-05 17:25:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,11 @@ var element=document.getElementById('main-text');
 element.innerHTML='new value';
 
 var img=document.getElementById('madi');
-img.onclick = function (){
-  img.style.marginLeft = '100px';  
+var marginLeft = 0;
+function moveRight () {
+  marginLeft = marginLeft + 10;
+  img.style.marginLeft = marginLLeft + 'px';
 };
+img.onclick = function (){
+  var interval = setInterval(moveRight, 100);
+};  
