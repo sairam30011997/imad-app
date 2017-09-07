346ce88a8e65081ad225d60f10089d9d71f3dd8d
sairam30011997 <31387961+sairam30011997@users.noreply.github.com>
2017-09-07 22:59:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,5 +34,5 @@ submit.onclick = function(){
         list += '<li>' + names[i] + '</li>';
     }
     var ul = document.getElementById('namelist');
-    ui.innerHTML = list;
+    ul.innerHTML = list;
 };
