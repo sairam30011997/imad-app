08c68e7f8bc221aaa456ee74a760493778cdfcaf
sairam30011997 <31387961+sairam30011997@users.noreply.github.com>
2017-09-12 23:29:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -132,9 +132,20 @@ app.get('/submit-name', function (req, res) {
     res.send(JSON.stringify(names));
 });
 
-app.get('/:articlename', function (req, res) {
-  var articlename = req.params.articlename;
-  res.send(createTemplate(articles[articlename]));
+app.get('/articles/:articlename', function (req, res) {
+  pool.query("SELECT * FROM article WHERE title = "+ req.params.articlename, function(err,result) {
+    if(err){
+        res.status(500).send(err.toString());
+    }  else{
+        if(result.rows.length === 0){
+            res.status(404).send('Article not found');
+        } else{
+            var articleData = results.rows[0];
+            res.send(createTemplate(articleData));
+        }
+    }
+  });
+  
 });
 
 app.get('/ui/style.css', function (req, res) {
