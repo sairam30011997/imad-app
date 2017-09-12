22f49f857456dffcacef582058c4840964fb879f
sairam30011997 <31387961+sairam30011997@users.noreply.github.com>
2017-09-12 14:27:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').pool;
-var app = express();
-app.use(morgan('combined'));
 
 var config = {
     user: 'sairamprakash16',
@@ -12,6 +10,9 @@ var config = {
     port: '5432',
     password: process.env.DB_PASSWORD
 };
+
+var app = express();
+app.use(morgan('combined'));
 
 var articles = {
     'article-one': {
