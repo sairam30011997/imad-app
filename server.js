fa6bcf4a5cc698b357c91c4494df152787c3f4ec
sairam30011997 <31387961+sairam30011997@users.noreply.github.com>
2017-09-14 12:28:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -140,7 +140,7 @@ app.get('/articles/:articleName', function (req, res) {
         if(result.rows.length === 0){
             res.status(404).send('Article not found');
         } else{
-            var articleData = results.rows[0];
+            var articleData = results.rows(0);
             res.send(createTemplate(articleData));
         }
     }
