01df410c5a8b70bbb956acd5cfadec3a7b4b37dd
sairam30011997 <31387961+sairam30011997@users.noreply.github.com>
2017-09-14 13:39:41 +0530

    [imad-console] Updates ui/main.js

---
+++ ui/main.js
@@ -0,0 +1,52 @@
+// counter code
+var button = document.getElementById('counter');
+button.onclick=function(){
+  
+  //create a request
+  var request = new XMLHttpRequest();
+  
+  //capture the response and store it in a variable
+  request.onreadystatechange = function(){
+    if(request.readyState === XMLHttpRequest.DONE){
+        if(request.status === 200){
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+    }
+    //not done yet
+  };
+  //make a request
+  request.open('GET','http://sairamprakash16.imad.hasura-app.io/counter',true);
+  request.send(null);
+};
+
+//submit name
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+     //create a request
+  var request = new XMLHttpRequest();
+  
+  //capture the response and store it in a variable
+  request.onreadystatechange = function(){
+    if(request.readyState === XMLHttpRequest.DONE){
+        if(request.status === 200){
+            //capture a list of names and render it as a list
+    var names = request.responseText;
+    names = JSON.parse(names);
+    var list = '';
+    for (var i=0; i < names.length; i++){
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+        }
+    }
+    //not done yet
+  };
+  //make a request
+  var nameInput = document.getElementById('name');
+  var name = nameInput.value;
+  request.open('GET','http://sairamprakash16.imad.hasura-app.io/submit-name?name=' + name,true);
+  request.send(null);
+};
